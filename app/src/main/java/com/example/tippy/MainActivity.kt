cc4bec223183e0d11bfb4d6121c3d8bb99ae5784
Pasindu Batagoda <pasindubatagoda@gmail.com>
2023-06-06 12:24:58 +0530

    seek bar, tip percent label

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.tippy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import org.w3c.dom.Text
 
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount: EditText
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+
+        //Assign initial tip value to seekbar and tip label
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+
+        //Seekbar change listener
+        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+             Log.i(TAG, "onProgressChanged $progress")
+                // "progress" is the seekbar value
+                // Update tip percentage based on seekbar value
+                tvTipPercentLabel.text ="$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
+        etBaseAmount.addTextChangedListener(object: TextWatcher{
+
+        })
     }
 }
