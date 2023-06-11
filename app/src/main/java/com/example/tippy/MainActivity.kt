85c1db0fbbfbc09aee1d7457a06a07b301d75c43
Pasindu Batagoda <pasindubatagoda@gmail.com>
2023-06-11 13:49:15 +0530

    Project complete

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tippy
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -8,7 +9,7 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.core.content.ContextCompat
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTipPercentLabel: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,12 +30,16 @@ class MainActivity : AppCompatActivity() {
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
 
-        //Assign initial tip value to seekbar and tip label
+        // Assign initial tip value to seekbar and tip label
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
 
-        //Seekbar change listener
+        // When app loads for the first time call this method and pass in INITIAL_TIP_PERCENT so there will be a tip description initially
+        updateTipDescription(INITIAL_TIP_PERCENT)
+
+        // Seekbar change listener
         seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.i(TAG, "onProgressChanged $progress")
@@ -41,6 +47,7 @@ class MainActivity : AppCompatActivity() {
                 // Update tip percentage based on seekbar value
                 tvTipPercentLabel.text = "$progress%"
                 computeTipAndTotal()
+                updateTipDescription(progress)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -49,7 +56,7 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        //Base bill amount change listener
+        // Base bill amount change listener
         etBaseAmount.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -61,6 +68,28 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    // Change tip description text based on tip percentage (poor, good, acceptable, etc)
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent) {
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"
+
+        }
+
+        tvTipDescription.text = tipDescription
+
+        // Update color based on tip percent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.color_worst_tip),
+            ContextCompat.getColor(this, R.color.color_best_tip)
+        ) as Int
+        tvTipDescription.setTextColor(color)
     }
 
     // when base amount is entered, calculate tip and total
@@ -89,6 +118,7 @@ class MainActivity : AppCompatActivity() {
          */
 
         tvTipAmount.text = "%.2f".format(tipAmount) // Format tip to 2 decimal places only
-        tvTotalAmount.text = "%.2f".format(totalAmount) // Format total amount to 2 decimal places only
+        tvTotalAmount.text =
+            "%.2f".format(totalAmount) // Format total amount to 2 decimal places only
     }
 }
